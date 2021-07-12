44e5aa1e481ade1a320302379ea899fe7a07bda5
William Barreto <bsi.william@gmail.com>
2021-07-12 14:14:18 -0300

    Adicionado ajustes em teste

--- src/main/java/br/com/prova/votacao/producer/SessaoEncerradaProducer.java
+++ src/main/java/br/com/prova/votacao/producer/SessaoEncerradaProducer.java
@@ -4,8 +4,6 @@ import br.com.prova.votacao.domain.ResultadoSessao;
 import br.com.prova.votacao.exception.ConversaoException;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
--- src/test/java/br/com/prova/votacao/service/GeradorResultadoSessaoServiceTest.java
+++ src/test/java/br/com/prova/votacao/service/GeradorResultadoSessaoServiceTest.java
@@ -3,22 +3,33 @@ package br.com.prova.votacao.service;
 import br.com.prova.votacao.IntegracaoTests;
 import br.com.prova.votacao.domain.ResultadoSessao;
 import br.com.prova.votacao.domain.Sessao;
+import br.com.prova.votacao.producer.SessaoEncerradaProducer;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
 
 public class GeradorResultadoSessaoServiceTest extends IntegracaoTests {
 
     @Autowired
     private GeradorResultadoSessaoService service;
 
+    @MockBean
+    private SessaoEncerradaProducer sessaoEncerradaProducer;
+
     @Test
     public void deve_gerar_resultado() {
+        // given
+        doNothing().when(sessaoEncerradaProducer).sendMessage(any());
         Sessao sessao = Sessao.builder().id(3L).build();
+        // when
         ResultadoSessao resultadoSessao = service.gerar(sessao);
+        // then
         assertThat(resultadoSessao.getId(), notNullValue());
         assertThat(resultadoSessao.getSessao().getId(), is(3L));
         assertThat(resultadoSessao.getQuantidadeVotosNao(), is(0));
